ddef69250ee0ebd03af476f45053f917346da7ad
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-04-08 05:53:11 +0000

    Complete join exercise

--- schema.sql
+++ schema.sql
@@ -10,3 +10,16 @@ CREATE TABLE animals(
 );
 
 ALTER TABLE animals ADD species varchar(100);
+
+-- EXERCISE 3
+
+CREATE TABLE owners (id SERIAL PRIMARY KEY, full_name VARCHAR(255), age INT);
+
+CREATE TABLE species (id SERIAL PRIMARY KEY, name VARCHAR(255))
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INT REFERENCES species (id);
+ALTER TABLE animals ADD owner_id INT REFERENCES owners (id);
+
+
+
