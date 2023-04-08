7a3f5c8f84fb1c1089604b41ba772d37c49d12d8
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-04-08 02:45:09 +0000

    Complete aggregate functions in postgresql assignment

--- queries.sql
+++ queries.sql
@@ -23,3 +23,70 @@ WHERE name != 'Gabumon';
 
 SELECT * FROM animals
 WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- TRANSACTIONS Exercise 2
+UPDATE animals
+SET species = 'unspecified';
+
+BEGIN TRANSACTION;
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE name NOT LIKE '%mon';
+
+COMMIT;
+
+BEGIN;
+DELETE FROM
+animals;
+ROLLBACK;
+select * from animals;
+
+
+BEGIN;
+
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT del1;
+
+UPDATE animals
+SET weight_kg = weight_kg * (-1);
+
+ROLLBACK TO del1;
+
+UPDATE animals
+SET weight_kg = weight_kg * (-1);
+
+COMMIT;
+
+
+-- Queries
+
+-- How many animals are there?
+SELECT count(*) FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT count(*) FROM animals
+WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT avg(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, count(*) FROM animals
+GROUP BY neutered;
+
+-- What is the minimum and maximum weight of each type of animal?
+
+SELECT min(weight_kg), max(weight_kg) FROM animals;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+
+SELECT species, avg(escape_attempts) FROM  animals
+WHERE date_part('year', date_of_birth) BETWEEN 1990 AND 2000
+GROUP BY species;
